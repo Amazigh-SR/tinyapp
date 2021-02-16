cfe6ede9a0587bbdad0f55787a18220a8414cc1a
Saïd Redouane <s_red009@hotmail.com>
2021-02-16 04:15:14 +0000

    Implemented a new route for the /urls/:shortURL endpoint

--- express_server.js
+++ express_server.js
@@ -22,9 +22,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-// app.get("/urls/:shortURL", (req, res) => {
-//   const templateVars = { shortURL: urlDatabase[shortURL] };
-// });
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  const templateVars = { shortURL: shortURL, longURL: longURL };
+  res.render("urls_show.ejs", templateVars);
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
