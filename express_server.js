661a2d674a0a2152260861a83fa4ebb1a0f223e2
Saïd Redouane <s_red009@hotmail.com>
2021-02-16 05:53:46 +0000

    Added a route for the /urls/new endpoint

--- express_server.js
+++ express_server.js
@@ -4,13 +4,19 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+//Our version of a DB
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+//List of potential routes that the server listens to:
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls", (req, res) => {
