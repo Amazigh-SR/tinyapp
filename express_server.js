672daa667c17355ab8ea11e99cdb5143e9e0b40c
Saïd Redouane <s_red009@hotmail.com>
2021-02-17 05:08:23 +0000

    Added the POST /logout endpoint (removed username cookie)

--- express_server.js
+++ express_server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 const PORT = 8080; // default port 8080
 
+//Setting the default view engine to ejs
 app.set("view engine", "ejs");
+//middleware cookieParser
+app.use(cookieParser());
 
 //A function that will generate a random string of 6 random alphanumeric characters
 const generateRandomString = function(length) {
@@ -50,16 +54,31 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
+//Need to consider the edge case where username is submited without a value
+app.post("/login", (req, res) => {
+  const userName = req.body.username;
+  res.cookie("username", userName);
+  res.redirect("/urls");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  }; // <-- Added cookie variable here
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] }; // <-- Added cookie variable here
   res.render("urls_index", templateVars);
 });
 
@@ -72,7 +91,11 @@ app.get("/u/:shortURL", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
-  const templateVars = { shortURL: shortURL, longURL: longURL };
+  const templateVars = {
+    shortURL: shortURL,
+    longURL: longURL,
+    username: req.cookies["username"], // <-- Added cookie variable here
+  };
 
   if (!longURL) {
     return res.status(404).send("Sorry, this resource is not available");
