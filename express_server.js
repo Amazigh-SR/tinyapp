dfe1af2df3b87fd625edcfcc6cb1825d6809c3a8
Saïd Redouane <s_red009@hotmail.com>
2021-02-17 23:33:24 +0000

    Added logic on the POST /register endpoint -i.e. dealing with cases where emails already exist and/or  empty emails/pws

--- express_server.js
+++ express_server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const PORT = 8080; // default port 8080
 
 //Setting the default view engine to ejs
 app.set("view engine", "ejs");
-//middleware cookieParser
+//npm module responsible for decrypting the buffer sent in the body of the request
+app.use(bodyParser.urlencoded({ extended: true }));
+//middleware cookieParser - similar to bodyParser but for cookies
 app.use(cookieParser());
 
+// ------------------ Helper Functions (BELOW)------------------//
 //A function that will generate a random string of 6 random alphanumeric characters
 const generateRandomString = function(length) {
   let result = "";
@@ -20,6 +24,18 @@ const generateRandomString = function(length) {
   return result;
 };
 
+//A function that will check if an id/email/password already exists in the database
+//ValueType --> Email, pw or userID; value --> Actual value;
+const verifyDB = function(valueType, value, db) {
+  for (const userID in db) {
+    if (db[userID][valueType] === value) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// -------------------- DataBases (BELOW)--------------------//
 //Our version of a users DB
 const users = {
   n2xsO1: {
@@ -40,13 +56,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-//npm module responsible for decrypting the buffer sent in the body of the request
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// ----------------- ROUTES (DOWN)-----------------//
-
+// -------------------- ROUTES (BELOW)--------------------//
 //List of potential routes that the server listens to:
+
+// ----------------- POST ROUTES (BELOW)-----------------//
 app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
   const shortURL = generateRandomString(6);
@@ -56,7 +69,19 @@ app.post("/urls", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
+  console.log(req.body);
   const { email, password } = req.body;
+  console.log(email, password);
+  //If the email or password fields are empty, notify the user
+  if (!email || !password) {
+    return res.status(400).send("Invalid email address or password");
+  }
+  //If email already exsists, notify the user
+  if (verifyDB("email", email, users)) {
+    return res.status(400).send("Email already exists");
+  }
+
+  //otherwise push the new user to the DB and set a cookie
   const user = generateRandomString(6);
   users[user] = { email, password, id: user };
   res.cookie("userID", user);
@@ -83,11 +108,12 @@ app.post("/logout", (req, res) => {
 
 //Need to consider the edge case where username is submited without a value
 app.post("/login", (req, res) => {
-  const userName = req.body.username;
-  res.cookie("username", userName);
+  // const userName = req.body.username;
+  // res.cookie("username", userName);
   res.redirect("/urls");
 });
 
+// ----------------- GET ROUTES (BELOW)-----------------//
 app.get("/register", (req, res) => {
   res.render("urls_registration.ejs");
 });
@@ -97,14 +123,21 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
+  const userID = users[req.cookies["userID"]];
+
   const templateVars = {
-    username: req.cookies["username"],
+    userID,
   }; // <-- Added cookie variable here
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] }; // <-- Added cookie variable here
+  const userID = users[req.cookies["userID"]];
+  const templateVars = {
+    urls: urlDatabase,
+    userID,
+  }; // <-- Added cookie variable here
+  console.log(req.cookies);
   res.render("urls_index", templateVars);
 });
 
@@ -117,10 +150,12 @@ app.get("/u/:shortURL", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
+  const userID = users[req.cookies["userID"]];
   const templateVars = {
-    shortURL: shortURL,
-    longURL: longURL,
-    username: req.cookies["username"], // <-- Added cookie variable here
+    shortURL,
+    longURL,
+    userID,
+    // <-- Added cookie variable here
   };
 
   if (!longURL) {
@@ -129,7 +164,7 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show.ejs", templateVars);
 });
 
-// ------------------ ROUTES (UP)------------------//
+// ------------------ Server Listens on PORT 8080 (BELOW)------------------//
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
