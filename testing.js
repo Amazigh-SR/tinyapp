69554d7f97884d2fa081a24257d36152397fb43f
Saïd Redouane <s_red009@hotmail.com>
2021-02-18 17:30:58 +0000

    Added the testing.js file in the .gitignore

--- testing.js
+++ testing.js
@@ -1,32 +1,17 @@
-const users = {
-  n2xsO1: {
-    id: "n2xsO",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  "8oL2xP": {
-    id: "oL2xP",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
+const urlDatabase = {
+  b6UTxQ: { longURL: "https://www.tsn.com", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ49lW" },
+  i3Boo0: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
 };
 
-const verifyDB = function(valueType, value, db) {
-  for (const userID in db) {
-    if (db[userID][valueType] === value) {
-      return userID;
+const urlsForUser = function(dbObj, userID) {
+  const obj = {};
+  for (const url in dbObj) {
+    if (userID === dbObj[url].userID) {
+      obj[url] = dbObj[url].longURL;
     }
   }
-  return false;
+  return obj;
 };
 
-console.log(verifyDB("email", "user2@example.com", users));
-
-const returnUserID = function(valueType, value, db) {
-  for (const userID in db) {
-    if (db[userID][valueType] === value) {
-      return userID;
-    }
-  }
-  return false;
-};
+console.log(urlsForUser(urlDatabase, "aJ48lW"));
